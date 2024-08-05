a09a4f78d20dd3fc9138d9bbf8966423f4f376a5
DANIEL23030 <PROLGP.DCACERES@GMAIL.COM>
2024-08-05 00:41:51 -0400

    Actualización de index, login y validaciones

--- login.php
+++ login.php
@@ -3,18 +3,23 @@ session_start();
 include 'config.php'; // Archivo con la configuración de la base de datos
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
     $password = $_POST['password'];
 
     // Consultar la base de datos para verificar las credenciales
-    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE username = ? AND password = ?");
-    $stmt->bind_param("ss", $username, $password);
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE username = ?");
+    $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-        $_SESSION['username'] = $username;
-        header("Location: index.php");
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['username'] = $username;
+            header("Location: index.php");
+        } else {
+            echo "Nombre de usuario o contraseña incorrectos.";
+        }
     } else {
         echo "Nombre de usuario o contraseña incorrectos.";
     }
